b06ff2aeb98490b08158a84e4cab2191806b2f6b
victorocna <victorocnarescu@gmail.com>
2025-04-02 20:27:49 +0300

    Update ignored comments

--- functions/prettify.js
+++ functions/prettify.js
@@ -1,8 +1,8 @@
 module.exports = (comment) => {
   try {
-    // Remove any shape comments like [%cal Ya7,Ya6], [%csl Ya7,Ya6]
-    // and clock comments like [%clk 3:00:00]
-    return comment.replace(/\s*\[(?:%c(?:al|sl)|%clk) [^\]]+\]/g, '').trim();
+    // Remove shape, clock, and eval comments
+    const commentRegex = /\s*\[%(?:cal|csl|clk|eval|evp) [^\]]+\]/g;
+    return comment.replace(commentRegex, '').trim();
   } catch (err) {
     return undefined;
   }
