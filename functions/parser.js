a97688318a335fbb713d410a26511c840ff27d33
victorocna <victor.ocnarescu@crediteurope.nl>
2025-05-22 12:24:07 +0200

    Upgrade chess.js package

--- functions/parser.js
+++ functions/parser.js
@@ -1,19 +1,18 @@
+const { Chess } = require('chess.js');
 const { initial } = require('./fen');
 const pgn = require('./pgn');
 const moment = require('./moment');
 
-module.exports = (chess, moves, fen = initial, depth = 1) => {
-  const loaded = chess.load_pgn(pgn.build(moves, fen));
-  if (!loaded) {
-    return [];
-  }
+module.exports = (moves, fen = initial, depth = 1) => {
+  const chess = new Chess();
+  chess.loadPgn(pgn.build(moves, fen)); // can throw if PGN is invalid
 
   const history = chess.history();
   while (chess.undo()) {
     chess.undo();
   }
 
-  const comment = chess.get_comment();
+  const comment = chess.getComment();
   const first = moment.build({ depth, comment, fen: chess.fen() });
 
   const moments = history.map((move) => {
@@ -22,14 +21,14 @@ module.exports = (chess, moves, fen = initial, depth = 1) => {
     return moment.build({
       depth,
       move,
-      comment: chess.get_comment(),
+      comment: chess.getComment(),
       fen: chess.fen(),
     });
   });
 
   // finally, add the first chess "moment" when needed
-  const header = chess.header();
-  if (header.FEN || fen === initial || first.comment || first.shapes) {
+  const headers = chess.getHeaders();
+  if (headers.FEN || fen === initial || first.comment || first.shapes) {
     moments.unshift(first);
   }
 
