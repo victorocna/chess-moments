868d3f16367822609ed1f8c048827599de3222d2
victorocna <victorocnarescu@gmail.com>
2025-06-27 19:03:02 +0300

    Remove duplicate exports

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,7 @@
 const fen = require('./fen');
 const flat = require('./flat');
-const getNextMoments = require('./extras/get-next-moments');
-const getPrevMoment = require('./extras/get-prev-moment');
 const lines = require('./lines');
 const moment = require('./moment');
-const momentsToPgn = require('./extras/moments-to-pgn');
 const parser = require('./parser');
 const pgn = require('./pgn');
 const prepare = require('./prepare');
@@ -17,11 +14,8 @@ const tree = require('./tree');
 module.exports = {
   fen,
   flat,
-  getNextMoments,
-  getPrevMoment,
   lines,
   moment,
-  momentsToPgn,
   parser,
   pgn,
   prepare,
