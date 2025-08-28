68d84eb92fb679ca0460a57c5ac15c84525c0700
victorocna <victorocnarescu@gmail.com>
2025-08-28 12:22:26 +0300

    Add new test for sidelines

--- tests/promote-mainline.test.js
+++ tests/promote-mainline.test.js
@@ -14,4 +14,21 @@ describe('Promote mainline', () => {
     // Assert
     expect(newMoments[3]?.move).to.equal('Nf3');
   });
+
+  it('Promote mainline: One sideline', () => {
+    // Arrange
+    const pgn = '1. e4 e5 (1... c5) 2. Nf3 Nc6 *';
+    const moments = flat(pgn);
+    const current = moments.find((m) => m.move === 'c5');
+
+    // Act
+    const newMoments = promoteMainline(moments, current);
+
+    // Assert
+    // Expected result: 1. e4 c5 (1.. e5 2. Nf3 Nc6)
+    expect(newMoments[2]?.move).to.equal('c5');
+    expect(newMoments[2]?.depth).to.equal(1);
+    expect(newMoments[4]?.move).to.equal('e5');
+    expect(newMoments[4]?.depth).to.equal(2);
+  });
 });
